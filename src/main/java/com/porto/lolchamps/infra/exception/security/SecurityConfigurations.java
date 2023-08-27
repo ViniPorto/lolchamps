6fd401c26f9855b7693ddc1116b4d15248f2df25
ViniPorto <leinateporto@gmail.com>
2023-08-27 20:03:17 -0300

    finalizado autenticação e autorização de rotas

--- src/main/java/com/porto/lolchamps/infra/exception/security/SecurityConfigurations.java
+++ src/main/java/com/porto/lolchamps/infra/exception/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package com.porto.lolchamps.infra.exception.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,15 +12,23 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(httpRequest -> httpRequest.requestMatchers(HttpMethod.POST, "/login").permitAll())
+                .authorizeHttpRequests(httpRequest -> httpRequest.requestMatchers(HttpMethod.GET, "/champ").permitAll())
+                .authorizeHttpRequests(httpRequest -> httpRequest.anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
--- src/main/java/com/porto/lolchamps/service/TokenService.java
+++ src/main/java/com/porto/lolchamps/service/TokenService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.porto.lolchamps.domain.user.Usuario;
 
 @Service
@@ -30,6 +31,19 @@ public class TokenService {
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token JWT", exception);
 }
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Lolchamps")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
     }
 
     private Instant dataExpiracao() {
