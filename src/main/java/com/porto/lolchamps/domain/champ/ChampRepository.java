7ecb19157e1064a08866b23f8c2653ccf3781afb
Vinícius Porto <leinateporto@gmail.com>
2024-05-22 21:18:03 -0300

    refactor querys repository

--- src/main/java/com/porto/lolchamps/domain/champ/ChampRepository.java
+++ src/main/java/com/porto/lolchamps/domain/champ/ChampRepository.java
@@ -25,26 +25,26 @@ public interface ChampRepository extends JpaRepository<Champ, Long> {
             """)
     Page<Champ> findAllFiltroPorName(Pageable paginacao, @Param("name") String name, @Param("sale") Boolean sale);
 
-    @Query(nativeQuery = true, value =  """
-            SELECT * FROM champ c
+    @Query(value =  """
+            SELECT c FROM Champ c
             WHERE c.ativo = true
             AND (
-                SELECT COUNT(cr.role_id) FROM champ_role cr
-                WHERE cr.champ_id = c.id
-                AND cr.role_id IN :roles
+                SELECT COUNT(cr.id) FROM ChampRole cr
+                WHERE cr.champ.id = c.id
+                AND cr.role.id IN :roles
             ) = :rolesSize
             AND (:sale is null OR c.sale = :sale)
             """)
 	Page<Champ> findAllFiltroPorRole(Pageable paginacao, @Param("roles") List<Long> roles, @Param("sale") Boolean sale, @Param("rolesSize") int size);
 
-    @Query(nativeQuery = true, value = """
-            SELECT * FROM champ c
+    @Query(value = """
+            SELECT c FROM champ c
             WHERE c.ativo = true
             AND c.name LIKE %:name%
             AND (
-                SELECT COUNT(cr.role_id) FROM champ_role cr
-                WHERE cr.champ_id = c.id
-                AND cr.role_id IN :roles
+                SELECT COUNT(cr.id) FROM ChampRole cr
+                WHERE cr.champ.id = c.id
+                AND cr.role.id IN :roles
             ) = :rolesSize
             AND (:sale is null OR c.sale = :sale)
             """)
